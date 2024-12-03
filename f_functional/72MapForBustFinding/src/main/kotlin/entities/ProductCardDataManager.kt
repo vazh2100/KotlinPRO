6f278fce45eec158d6738b7ea0ae69a8ef5816bd
vazh2100 <vazh2100@list.ru>
2024-12-03 20:32:40 +0300

    Functional: 72MapForBust

---
+++ f_functional/72MapForBustFinding/src/main/kotlin/entities/ProductCardDataManager.kt
@@ -0,0 +1,23 @@
+package com.vazh2100.entities
+
+import kotlinx.serialization.json.Json
+import java.io.File
+
+object ProductCardDataManager {
+    private val productCardFile = File("product_cards.json")
+
+
+    private val _productCards: MutableList<ProductCard> = loadProducts()
+    val productCards get() = _productCards.toList()
+
+
+    private fun loadProducts(): MutableList<ProductCard> {
+        val content = productCardFile
+            .readText()
+            .trim()
+        val json = Json {
+            ignoreUnknownKeys = true
+        }
+        return json.decodeFromString(content)
+    }
+}
