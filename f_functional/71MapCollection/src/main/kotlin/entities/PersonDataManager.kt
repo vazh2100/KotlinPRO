16850b6a91ff4f687175ba4580b2c6a4b6d59b34
vazh2100 <vazh2100@list.ru>
2024-12-03 19:18:18 +0300

    Functional: 71MapCollection

---
+++ f_functional/71MapCollection/src/main/kotlin/entities/PersonDataManager.kt
@@ -0,0 +1,21 @@
+package com.vazh2100.entities
+
+import kotlinx.serialization.json.Json
+import java.io.File
+
+object PersonDataManager {
+    private val profilesFile = File("profiles.json")
+
+
+    private val _profiles: MutableList<Person> = loadProfiles()
+    val profiles get() = _profiles.toList()
+
+
+    private fun loadProfiles(): MutableList<Person> {
+        val content = profilesFile
+            .readText()
+            .trim()
+        return Json.decodeFromString(content)
+
+    }
+}
