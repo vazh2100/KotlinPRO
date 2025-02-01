6bfad505c6328bf0e85ffda0b60d259f58c40d22
vazh2100 <vazh2100@list.ru>
2025-02-01 10:34:19 +0300

    j_details OOP and Functions: Destruction and Infix

---
+++ h_collections/src/main/kotlin/j_details/DestructionAndInfix.kt
@@ -0,0 +1,17 @@
+package j_details
+
+fun main() {
+    val dictionary = listOf(
+        "Hello" to "Bonjour", "Thank you" to "Merci"
+    )
+
+    // Класс Pair распадается на 2 переменные
+    for ((first, second) in dictionary) {
+        println("$first - $second")
+    }
+
+}
+
+data class MyPair<F, S>(val first: F, val second: S)
+
+infix fun <F, S> F.to(second: S) = MyPair(this, second)
