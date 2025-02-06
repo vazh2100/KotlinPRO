38406cc48c50fc8a5597ebd6b8c64bf1accdc4be
vazh2100 <vazh2100@list.ru>
2025-02-06 12:25:52 +0300

    Async and MultiThreading: Dispatcher Default and Unconfined

--- i_async/src/main/kotlin/b_coroutines/Display.kt
+++ i_async/src/main/kotlin/b_coroutines/Display.kt
@@ -13,37 +13,37 @@ import kotlin.concurrent.thread
 // По сути механизм coroutines - это callbacks
 object Display {
 
-    private val scope = CoroutineScope(CoroutineName("MyName"))
-
+    private val scope = CoroutineScope(CoroutineName("MyName") + Dispatchers.Unconfined)
     private val infoArea = JTextArea().apply {
         isEditable = false
     }
-
     private val loadButton = JButton("Load book info").apply {
         addActionListener {
             // Создаём Coroutine. "launch" - это один из строителей корутин.
             scope.launch {
+                println("Started in thread: ${Thread.currentThread().name}")
+                delay(100)
+                println("Resumed in thread: ${Thread.currentThread().name}")
                 isEnabled = false
                 infoArea.text = "Loading book information...\n"
                 val book = loadBook()
+                println("Resumed in thread: ${Thread.currentThread().name}")
                 println(book)
                 infoArea.append(book.toString())
                 infoArea.append("Loading author information...\n")
                 val author = loadAuthor(book)
+                println("Resumed in thread: ${Thread.currentThread().name}")
                 println(author)
                 infoArea.append(author.toString())
                 isEnabled = true
             }
         }
     }
-
     private val timerLabel = JLabel("Time: 00:00")
-
     private val topPanel = JPanel(BorderLayout()).apply {
         add(timerLabel, BorderLayout.WEST)
         add(loadButton, BorderLayout.EAST)
     }
-
     private val mainFrame = JFrame("Book and Author info").apply {
         layout = BorderLayout()
         add(topPanel, BorderLayout.NORTH)
@@ -61,13 +61,20 @@ object Display {
         startTimer()
     }
 
+    private suspend fun longOperation() = withContext(Dispatchers.Default) {
+        val a = mutableListOf<Int>()
+        repeat(300_000) {
+            a.add(0, it)
+        }
+    }
+
     private suspend fun loadBook(): Book {
-        delay(3000)
+        longOperation()
         return Book("1984", 1949, "Dystopia")
     }
 
     private suspend fun loadAuthor(book: Book): Author {
-        delay(3000)
+        longOperation()
         return Author("George Orwell", "British writer and journalist")
     }
 
@@ -90,8 +97,12 @@ object Display {
 fun main() {
     Display.show()
 }
-
 // Structured Concurrency - Структурированный параллелизм
 // 1) Корутины должны быть запущены внутри области с определенным жизненным циклом.
 // Это означает, что если нам больше не нужна область(вернулись на предыдущий экран, например), вся корутины области
 // должны быть отменены.
+//
+// Dispatcher-Default использует пул потоков фиксированной длины равный количеству физических ядер процессора
+// Dispatcher-Unconfined запускает корутины в том потоке на котором её создали, но после приостановки дальнейшее выполнение
+// зависит от приостанавливающей функции
+// Coroutine context - это составное понятие. В него входят Имя, Dispatcher.
