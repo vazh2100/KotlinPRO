5080e70262766d192ccc59178ec0a95b4488ab6b
vazh2100 <vazh2100@list.ru>
2025-02-09 11:29:00 +0300

    Async and MultiThreading: Exceptions part 1

---
+++ i_async/src/main/kotlin/g_exceptions/Main.kt
@@ -0,0 +1,37 @@
+package g_exceptions
+
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.asCoroutineDispatcher
+import kotlinx.coroutines.launch
+import java.util.concurrent.Executors
+
+private val dispatcher = Executors.newCachedThreadPool().asCoroutineDispatcher()
+private val handler = CoroutineExceptionHandler { context, throwable ->
+    println(throwable)
+}
+private val scope = CoroutineScope(dispatcher + handler)
+
+fun main() {
+    val d = scope.launch {
+        method()
+        try {
+            method()
+        } catch (e: Exception) {
+            println("yes")
+        }
+    }
+    Thread.sleep(5000)
+    scope.launch {
+        println("Не выводится")
+    }
+    Thread.sleep(5000)
+}
+
+suspend fun method(): String {
+    error("Error")
+}
+// если в дочерней корутине было брошено исключение, то оно обрушивает весь scope.
+// Чтобы обработать исключение в корневой Job, нужно добавить CoroutineExceptionHandler.
+// Но это не оживляет scope. корневой Job отменяет все дочерние Coroutine
+// CoroutineExceptionHandler - это ещё одна составляющая корутин context
